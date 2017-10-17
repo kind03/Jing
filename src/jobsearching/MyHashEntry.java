90b4c7565b3d47014cf24c7efe645a4e8b0610b7
Jing <hejing194@hotmail.com>
2017-10-17 21:51:06 +0800

    HashSet modified to HashMap

--- src/jobsearching/MyHashEntry.java
+++ src/jobsearching/MyHashEntry.java
@@ -1,31 +1,39 @@
 package jobsearching;
 
-public class MyHashEntry <T> {
-	private T content;
-	private MyHashEntry<T> next;
-	public MyHashEntry<T> getNext() {
+public class MyHashEntry <T,E> {
+	private T key;
+	private E value;
+	private MyHashEntry<T, E> next;
+	public MyHashEntry(T key, E value) {
+		this.key = key;
+		this.value = value;
+	}
+	public MyHashEntry<T, E> getNext() {
 		return next;
 	}
-	public MyHashEntry(T content) {
-		this.content = content;
-	}
-	public void setNext(MyHashEntry<T> next) {
+	public void setNext(MyHashEntry<T,E> next) {
 		this.next = next;
 	}
-	public T getContent() {
-		return content;
+	public T getKey() {
+		return key;
 	}
-	public void setContent(T content) {
-		this.content = content;
+	public void setKey(T key) {
+		this.key = key;
+	}
+	public E getValue() {
+		return value;
+	}
+	public void setValue(E value) {
+		this.value = value;
 	}
 	public String toString(){
 		StringBuilder sb = new StringBuilder("[");
-		MyHashEntry<T> pointer = this;
-		sb.append(pointer.getContent());
+		MyHashEntry<T,E> pointer = this;
+		sb.append("("+pointer.getKey()+","+pointer.getValue()+")");
 		while (pointer.getNext() != null){
 			pointer = pointer.getNext();
 			sb.append(",");
-			sb.append(pointer.getContent());
+			sb.append("("+pointer.getKey()+","+pointer.getValue()+")");
 		}
 		sb.append("]");
 		return sb.toString();
--- src/jobsearching/MyHashSet.java
+++ src/jobsearching/MyHashSet.java
@@ -1,14 +1,15 @@
 package jobsearching;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class MyHashSet <T>{
+public class MyHashSet <T,E>{
 	private int size=0;
 	private int bucketSize=0;
 //	Actually this bucket should be a integer or long array;
 //	the index numbers should be the bucket number,
 //	and the values should be the memory address of the element in Hashset
-	private MyHashEntry<T>[] buckets;
+	private MyHashEntry<T,E>[] buckets;
 	public MyHashSet(){
 		bucketSize = 12;
 		buckets = new MyHashEntry[bucketSize];
@@ -19,31 +20,31 @@ public class MyHashSet <T>{
 		buckets = new MyHashEntry[bucketSize];
 //		buckets = (T[]) new Object[bucketSize];
 	}
-	public void add(T element){
+	public void put(T key, E value){
 //		Linked List Length count. 
 //		when reaches certain number, will activate resize procedure.
 		int i=1;
-		MyHashEntry<T> e1 = new MyHashEntry<T>(element);
+		MyHashEntry<T,E> e1 = new MyHashEntry<T,E>(key,value);
 //		elements cannot be the same in a set
 		boolean duplicated = false;
-		int hashNo = element.hashCode() % bucketSize;
+		int hashNo = key.hashCode() % bucketSize;
 		if (buckets[hashNo]==null){
 			buckets[hashNo]=e1;	
 		} else {
 //			deal with bucket conflict
 
 //			deal with the first element in the linked list
-			MyHashEntry<T> pointer = buckets[hashNo];
-			if (pointer.getContent().equals(element)) duplicated=true;
+			MyHashEntry<T,E> pointer = buckets[hashNo];
+			if (pointer.getKey().equals(key)) duplicated=true;
 //			use while loop to move to the end of the Linked List
 			while (pointer.getNext()!=null){
 				pointer = pointer.getNext();
-				if (pointer.getContent().equals(element)) duplicated=true;
+				if (pointer.getKey().equals(key)) duplicated=true;
 				i++;
 			}
 //			append the new element to the end of the linked list
 			if (duplicated==false){
-				pointer.setNext(new MyHashEntry<T>(element));
+				pointer.setNext(new MyHashEntry<T,E>(key,value));
 				i++;
 				}
 		}
@@ -59,29 +60,29 @@ public class MyHashSet <T>{
 		int oldBucketSzie = bucketSize;
 		this.bucketSize = newSize;
 		size=0;
-		MyHashEntry<T>[] oldBuckets = this.buckets;
+		MyHashEntry<T,E>[] oldBuckets = this.buckets;
 		buckets = new MyHashEntry[bucketSize];
 //		traverse each buckets 
 		for(int i=0;i<oldBucketSzie;i++){
-			MyHashEntry<T> pointer = oldBuckets[i];
+			MyHashEntry<T,E> pointer = oldBuckets[i];
 			while(pointer!=null){
-				this.add(pointer.getContent());
+				this.put(pointer.getKey(),pointer.getValue());
 				pointer = pointer.getNext();
 			}
 		}
 	}
-	public T[] toArray(){
-		T[] array = (T[]) new Object[size];
+	public String[] toArray(){
+		String[] array = new String[size];
 		int no=0;
 		for(int i=0;i<bucketSize;i++){
 			if (buckets[i]!=null){
-				array[no]=buckets[i].getContent();
+				array[no]=buckets[i].getKey()+"="+buckets[i].getValue();
 				no=no+1;
 //		Traverse the linked list in the same bucket forming when bucket conflict happens
-				MyHashEntry<T> pointer = buckets[i];
+				MyHashEntry<T,E> pointer = buckets[i];
 				while(pointer.getNext()!=null){
 					pointer = pointer.getNext();
-					array[no]=pointer.getContent();
+					array[no]=pointer.getKey()+"="+buckets[i].getValue();
 					no=no+1;
 				}
 			}
@@ -95,25 +96,28 @@ public class MyHashSet <T>{
 		return size;
 	}
 	public static void main(String[] args) {
-		MyHashSet<Integer> mhs = new MyHashSet<Integer>();
-		mhs.add(0);
-		mhs.add(1);
-		mhs.add(2);
-		mhs.add(12);
-		mhs.add(13);
-		mhs.add(14);
-		mhs.add(24);
-		mhs.add(36);
-		mhs.add(48);
-		mhs.add(60);
-		mhs.add(72);
+		MyHashSet<Integer,Integer> mhs = new MyHashSet<Integer,Integer>();
+		mhs.put(0,1);
+		mhs.put(1,1);
+		mhs.put(2,1);
+		mhs.put(3,1);
+		mhs.put(14,1);
+		mhs.put(13,1);
+		mhs.put(12,1);
+		mhs.put(24,1);
+		mhs.put(36,1);
+		mhs.put(48,1);
+		mhs.put(60,1);
+		mhs.put(72,1);
 		System.out.println(mhs);
 		System.out.println(mhs.size());
-		mhs.add(96);
-		mhs.add(84);
-		mhs.add(1);
-		mhs.add(2);
+		mhs.put(96,1);
+		mhs.put(84,1);
+		mhs.put(1,1);
 		System.out.println(mhs);
 		System.out.println(mhs.size());
+		HashMap<Integer,Integer> mh = new HashMap<Integer,Integer>();
+		mh.put(1, 2);
+		
 	}
 }
